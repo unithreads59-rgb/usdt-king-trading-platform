0fb07bce7a7fb3673db6b2bb937d01f580ba01c9
unithreads59-rgb <unithreads59@gmail.com>
2025-08-06 19:45:49 +0530

    Add .next/static/chunks/src_app_admin_dashboard_page_tsx_a993527f._.js 1754489747672

---
+++ .next/static/chunks/src_app_admin_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_077ef4f7._.js",
+  "static/chunks/node_modules_bc123df9._.js"
+],
+    source: "dynamic"
+});
